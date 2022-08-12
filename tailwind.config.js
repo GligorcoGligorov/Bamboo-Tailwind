cbd6b9a0c54fbc56beeea6dc73e75ef07ad121f3
Gligorco Gligorov <ggligorov1709@gmail.com>
2022-08-12 19:29:23 +0200

    Bamboo website with responsive design - Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        merriweather: "'Merriweather', serif;",
+        inter: "'Inter', sans- serif;"
+        
+      }
+    },
+    fontFamily: {
+      body: ['']
+    }
+  },
+  plugins: [],
+}
